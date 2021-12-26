afdb7a9a0ba6ad0f3171653971bc67701563f861
Evelym Rondon <evionline@gmail.com>
2021-12-26 19:30:20 -0400

    Métodos getters and setters

--- ExemploConstrutores/Program.cs
+++ ExemploConstrutores/Program.cs
@@ -14,11 +14,18 @@ namespace ExemploConstutores
       // Log log2 = Log.GetInstance();
       // System.Console.WriteLine(log2.PropriedadeLog);
 
-      Pessoa p1 = new Pessoa("Evelym", "Pereira");
-      p1.Apresentar();
+      // Pessoa p1 = new Pessoa("Evelym", "Pereira");
+      // p1.Apresentar();
 
-      Aluno a1 = new Aluno("Camile", "da Silva", "Matemática");
-      a1.Apresentar();
+      // Aluno a1 = new Aluno("Camile", "da Silva", "Matemática");
+      // a1.Apresentar();
+
+      Data dt = new Data();
+      // dt.Setmes(20);
+      dt.Mes = 20;
+      System.Console.WriteLine(dt.Mes);
+      dt.ApresentarMes();
+
     }
   }
 }
